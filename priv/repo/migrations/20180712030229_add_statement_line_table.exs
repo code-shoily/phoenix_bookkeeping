ba4fb21900acd4304aa72634cfe9218d061d06f1
mafinar <mafinar@gmail.com>
2018-07-11 23:51:15 -0400

    Add fields to statement line table

--- priv/repo/migrations/20180712030229_add_statement_line_table.exs
+++ priv/repo/migrations/20180712030229_add_statement_line_table.exs
@@ -2,6 +2,17 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddStatementLineTable do
   use Ecto.Migration
 
   def change do
+    create table(:statement_line, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:date, :utc_datetime)
+      add(:statement_import, references(:statement_import))
+      add(:amount, :decimal)
+      add(:description, :string)
+      add(:type, :string)
+      add(:transaction, references(:transaction))
+      add(:source_data, :jsonb)
 
+      timestamps()
+    end
   end
 end
