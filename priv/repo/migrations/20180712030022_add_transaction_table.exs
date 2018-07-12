70d565deef4683a5428ac04444676c6479352a0d
mafinar <mafinar@gmail.com>
2018-07-11 23:44:58 -0400

    Add fields to transaction table

--- priv/repo/migrations/20180712030022_add_transaction_table.exs
+++ priv/repo/migrations/20180712030022_add_transaction_table.exs
@@ -2,6 +2,11 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddTransactionTable do
   use Ecto.Migration
 
   def change do
-
+    create table(:transaction, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:date, :utc_datetime)
+      add(:description, :string)
+      timestamps()
+    end
   end
 end
