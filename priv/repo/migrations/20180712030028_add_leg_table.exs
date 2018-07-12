1506757a2ac06b7e4c4d3c6168af7b2669cf5a1f
mafinar <mafinar@gmail.com>
2018-07-11 23:47:14 -0400

    Add fields to transaction table

--- priv/repo/migrations/20180712030028_add_leg_table.exs
+++ priv/repo/migrations/20180712030028_add_leg_table.exs
@@ -2,6 +2,14 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddLegTable do
   use Ecto.Migration
 
   def change do
+    create table(:leg, primary_key: false) do
+      add(:id, :uuid, primary_key: false)
+      add(:transaction, references(:transaction))
+      add(:account, references(:account))
+      add(:amount, :decimal)
+      add(:description, :string)
 
+      timestamps()
+    end
   end
 end
