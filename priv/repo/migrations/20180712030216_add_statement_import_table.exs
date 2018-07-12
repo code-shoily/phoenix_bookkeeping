0baa0c4cb50696068ce1265ea7c9fb6ca008c9ec
mafinar <mafinar@gmail.com>
2018-07-11 23:49:01 -0400

    Add fields to statement_import table

--- priv/repo/migrations/20180712030216_add_statement_import_table.exs
+++ priv/repo/migrations/20180712030216_add_statement_import_table.exs
@@ -2,6 +2,13 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddStatementImportTable do
   use Ecto.Migration
 
   def change do
+    create table(:statement_import, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:bank_account, references(:account))
+      add(:source, :string, size: 50)
+      add(:extra, :jsonb)
 
+      timestamps()
+    end
   end
 end
