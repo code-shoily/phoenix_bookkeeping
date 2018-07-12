b0d6848fdef885e9dd096c04b01387a1edbbb391
mafinar <mafinar@gmail.com>
2018-07-12 19:32:26 -0400

    Add some constraints on Account Table

--- priv/repo/migrations/20180712030016_add_account_table.exs
+++ priv/repo/migrations/20180712030016_add_account_table.exs
@@ -4,11 +4,11 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddAccountTable do
   def change do
     create table(:account, primary_key: false) do
       add(:id, :uuid, primary_key: true)
-      add(:name, :string, size: 50)
-      add(:code, :string, size: 3)
-      add(:full_code, :string, size: 100)
-      add(:type, :string, size: 10)
-      add(:is_bank_account, :boolean)
+      add(:name, :string, size: 50, null: false)
+      add(:code, :string, size: 3, null: false)
+      add(:full_code, :string, size: 100, null: false)
+      add(:type, :string, size: 10, null: false)
+      add(:is_bank_account, :boolean, default: false)
       add(:currencies, {:array, :string})
       timestamps()
     end
