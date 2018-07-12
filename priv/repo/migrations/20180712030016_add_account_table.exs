7c4723d2085a4ecf7239bf22fba7d2e4d71f6b40
mafinar <mafinar@gmail.com>
2018-07-11 23:17:41 -0400

    Add account table fields

--- priv/repo/migrations/20180712030016_add_account_table.exs
+++ priv/repo/migrations/20180712030016_add_account_table.exs
@@ -2,6 +2,14 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddAccountTable do
   use Ecto.Migration
 
   def change do
-
+    create table(:account, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:name, :string, size: 50)
+      add(:code, :string, size: 3)
+      add(:full_code, :string, size: 100)
+      add(:type, :string, size: 10)
+      add(:is_bank_account, :boolean)
+      add(:currencies, {:array, :string})
+    end
   end
 end
