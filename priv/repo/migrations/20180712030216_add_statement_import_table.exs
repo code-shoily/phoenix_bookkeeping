0f22693576ce8ab2570f5c451db14e807a327938
mafinar <mafinar@gmail.com>
2018-07-11 23:02:48 -0400

    Add skeleton of migrations for auxiliary tables

---
+++ priv/repo/migrations/20180712030216_add_statement_import_table.exs
@@ -0,0 +1,7 @@
+defmodule PhoenixBookkeeping.Repo.Migrations.AddStatementImportTable do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
---
+++ priv/repo/migrations/20180712030229_add_statement_line_table.exs
@@ -0,0 +1,7 @@
+defmodule PhoenixBookkeeping.Repo.Migrations.AddStatementLineTable do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
