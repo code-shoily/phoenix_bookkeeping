3b530d3c33bc8a0497930bd815739aba5b50fd3e
mafinar <mafinar@gmail.com>
2018-07-12 19:26:48 -0400

    Fix references with uuid

--- priv/repo/migrations/20180712030028_add_leg_table.exs
+++ priv/repo/migrations/20180712030028_add_leg_table.exs
@@ -3,9 +3,9 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddLegTable do
 
   def change do
     create table(:leg, primary_key: false) do
-      add(:id, :uuid, primary_key: false)
-      add(:transaction, references(:transaction))
-      add(:account_id, references(:account))
+      add(:id, :uuid, primary_key: true)
+      add(:transaction_id, references(:transaction, type: :uuid))
+      add(:account_id, references(:account, type: :uuid))
       add(:amount, :decimal, precision: 13, scale: 2)
       add(:description, :string)
 
