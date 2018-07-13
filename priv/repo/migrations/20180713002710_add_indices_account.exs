30c11162ab1b84bc656912ba1bf620d6c399fdf5
mafinar <mafinar@gmail.com>
2018-07-12 20:38:33 -0400

    Add unique indices

--- priv/repo/migrations/20180713002710_add_indices_account.exs
+++ priv/repo/migrations/20180713002710_add_indices_account.exs
@@ -5,5 +5,8 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddIndicesAccount do
     create(index(:account, [:parent_id]))
     create(index(:account, [:full_code]))
     create(index(:account, [:currencies]))
+
+    create(unique_index(:account, [:full_code], name: "account_full_code_unique_index"))
+    create(unique_index(:account, [:parent_id, :code], name: "account_parent_id_unique_index"))
   end
 end
