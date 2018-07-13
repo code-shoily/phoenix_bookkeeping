9e24bdb76a1fb472ac9ebba43782b94d8d90ea38
mafinar <mafinar@gmail.com>
2018-07-12 20:31:00 -0400

    Bugfixed

--- priv/repo/migrations/20180713002710_add_indices_account.exs
+++ priv/repo/migrations/20180713002710_add_indices_account.exs
@@ -2,8 +2,8 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddIndicesAccount do
   use Ecto.Migration
 
   def change do
-    create(index("parent", [:parent_id], prefix: "idx"))
-    create(index("full_code", [:full_code], prefix: "idx"))
-    create(index("currencies", [:currencies], prefix: "idx"))
+    create(index("parent_id", [:parent_id]))
+    create(index("full_code", [:full_code]))
+    create(index("currencies", [:currencies]))
   end
 end
