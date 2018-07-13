03d72430696a291846f68e2c005f366cc9605ab0
mafinar <mafinar@gmail.com>
2018-07-12 20:34:04 -0400

    Properly do create index

--- priv/repo/migrations/20180713002710_add_indices_account.exs
+++ priv/repo/migrations/20180713002710_add_indices_account.exs
@@ -2,8 +2,8 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddIndicesAccount do
   use Ecto.Migration
 
   def change do
-    create(index("parent_id", [:parent_id]))
-    create(index("full_code", [:full_code]))
-    create(index("currencies", [:currencies]))
+    create(index(:account, [:parent_id]))
+    create(index(:account, [:full_code]))
+    create(index(:account, [:currencies]))
   end
 end
