a1fa86199cd50c0b4f11766bfb9c89431e3ad9be
mafinar <mafinar@gmail.com>
2018-07-12 20:03:43 -0400

    Add some constraints on Leg Table

--- priv/repo/migrations/20180712030028_add_leg_table.exs
+++ priv/repo/migrations/20180712030028_add_leg_table.exs
@@ -4,9 +4,9 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddLegTable do
   def change do
     create table(:leg, primary_key: false) do
       add(:id, :uuid, primary_key: true)
-      add(:transaction_id, references(:transaction, type: :uuid))
-      add(:account_id, references(:account, type: :uuid))
-      add(:amount, :decimal, precision: 13, scale: 2)
+      add(:transaction_id, references(:transaction, type: :uuid), null: false)
+      add(:account_id, references(:account, type: :uuid), null: false)
+      add(:amount, :decimal, precision: 13, scale: 2, null: false)
       add(:description, :string)
 
       timestamps()
