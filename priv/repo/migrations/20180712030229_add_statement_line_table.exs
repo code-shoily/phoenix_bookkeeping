e8dafd2812ca55c29da2194dfbbf5a76c1edc363
mafinar <mafinar@gmail.com>
2018-07-12 20:05:11 -0400

    Add some constraints on StatementLine Table

--- priv/repo/migrations/20180712030229_add_statement_line_table.exs
+++ priv/repo/migrations/20180712030229_add_statement_line_table.exs
@@ -4,12 +4,12 @@ defmodule PhoenixBookkeeping.Repo.Migrations.AddStatementLineTable do
   def change do
     create table(:statement_line, primary_key: false) do
       add(:id, :uuid, primary_key: true)
-      add(:date, :utc_datetime)
-      add(:statement_import_id, references(:statement_import, type: :uuid))
-      add(:amount, :decimal, precision: 13, scale: 2)
+      add(:date, :utc_datetime, default: fragment("NOW()"))
+      add(:statement_import_id, references(:statement_import, type: :uuid), null: false)
+      add(:amount, :decimal, precision: 13, scale: 2, null: false)
       add(:description, :string)
-      add(:type, :string)
-      add(:transaction_id, references(:transaction, type: :uuid))
+      add(:type, :string, null: false)
+      add(:transaction_id, references(:transaction, type: :uuid), null: false)
       add(:source_data, :jsonb)
 
       timestamps()
